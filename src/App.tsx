75632b137ecb1ff189381c2dc779adfa94c4b539
ecomnazar <ecomnazar.me@gmail.com>
2023-12-16 11:46:55 +0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <main className="w-screen h-screen flex items-center justify-center">
-      <h1 className="text-[60px] font-bold text-blue-500">Efir</h1>
+      <h1 className="text-[60px] font-bold text-green-500">Efir</h1>
     </main>
   )
 }
