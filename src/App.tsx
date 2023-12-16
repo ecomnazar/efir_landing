f7b03447685958d4a847458cfc2d670ebbb9b201
ecomnazar <ecomnazar.me@gmail.com>
2023-12-16 10:04:49 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <main className="w-screen h-screen flex items-center justify-center">
+      <h1 className="text-[60px] font-bold text-blue-500">Efir</h1>
+    </main>
+  )
+}
+
+export default App
